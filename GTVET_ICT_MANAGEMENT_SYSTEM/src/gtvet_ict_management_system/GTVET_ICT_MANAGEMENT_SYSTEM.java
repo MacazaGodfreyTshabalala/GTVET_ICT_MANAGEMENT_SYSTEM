1d8be2cfb1848821745a22290aa5432b91f55370
Macaza Tshabalala <macazatshabalala726@gmail.com>
2025-08-27 15:22:50 +0200

    CREATE A NEW PROJECT

---
+++ GTVET_ICT_MANAGEMENT_SYSTEM/src/gtvet_ict_management_system/GTVET_ICT_MANAGEMENT_SYSTEM.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package gtvet_ict_management_system;
+
+/**
+ *
+ * @author Tshabalala.M
+ */
+public class GTVET_ICT_MANAGEMENT_SYSTEM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
