a247e353867475a7c05428a0b7ee1568ddce1ebf
Macaza Tshabalala <macazatshabalala726@gmail.com>
2025-08-27 16:15:42 +0200

    create login frame

--- GTVET_ICT_MANAGEMENT_SYSTEM/src/gtvet_ict_management_system/GTVET_ICT_MANAGEMENT_SYSTEM.java
+++ GTVET_ICT_MANAGEMENT_SYSTEM/src/gtvet_ict_management_system/GTVET_ICT_MANAGEMENT_SYSTEM.java
@@ -1,20 +1,17 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package gtvet_ict_management_system;
 
-/**
- *
- * @author Tshabalala.M
- */
+
 public class GTVET_ICT_MANAGEMENT_SYSTEM {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         // TODO code application logic here
+        Login LoginFrame =   new Login();
+        LoginFrame.setVisible(true);
+          
     }
+  
+
     
 }
